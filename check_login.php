6ccaa0c0e9bfb0e1fcdc1916b4cef16be332c039
Worrameth <worrameth666@gmail.com>
2023-12-30 17:52:27 +0700

    add DockerFile and fix config

--- check_login.php
+++ check_login.php
@@ -1,35 +1,30 @@
 <?php
-	// session_start();
+// session_start();
 
-	require_once("connect.php");
-	
-	$stremp_username = mysqli_real_escape_string($conn,$_POST['username']);
-	$stremp_userpass = mysqli_real_escape_string($conn,$_POST['password']);
+require_once("connect.php");
 
-	$strSQL = "SELECT * FROM employee WHERE username = '".$stremp_username."' AND password = '".$stremp_userpass."'";
-	$objQuery = mysqli_query($conn,$strSQL);
-	$objResult = mysqli_fetch_array($objQuery);
-	if(!$objResult)
-	{
-		echo "<script language=\"JavaScript\">";
-		echo "alert('ชื่อผู้ใช้และรหัสผ่านของคุณไม่ถูกต้อง กรุณาลองใหม่อีกครั้ง');window.location='index.php'";
-		echo "</script>";
+$stremp_username = mysqli_real_escape_string($conn, $_POST['username']);
+$stremp_userpass = mysqli_real_escape_string($conn, $_POST['password']);
+
+$strSQL = "SELECT employee.username, employee.phone, department.departmentName, employee.role FROM employee 
+	LEFT JOIN department ON employee.departmentId = department.departmentId WHERE username = '" . $stremp_username . "' AND password = '" . $stremp_userpass . "'";
+$objQuery = mysqli_query($conn, $strSQL);
+$objResult = mysqli_fetch_array($objQuery);
+if (!$objResult) {
+	echo "<script language=\"JavaScript\">";
+	echo "alert('ชื่อผู้ใช้และรหัสผ่านของคุณไม่ถูกต้อง กรุณาลองใหม่อีกครั้ง');window.location='index.php'";
+	echo "</script>";
+} else {
+	$_SESSION["username"] = $objResult["username"];
+	$_SESSION["phone"] = $objResult["phone"];
+	$_SESSION["departmentName"] = $objResult["departmentName"];
+	if ($objResult["role"] == "HR") {
+		header("location:HR/index.php");
+	} elseif ($objResult["role"] == "Manager") {
+		header("location:Manager/index.php");
+	} else {
+		header("location:Employee/index.php");
 	}
-	else
-	{
-			$_SESSION["username"] = $objResult["username"];
-			$_SESSION["phone"] = $objResult["phone"];
-			$_SESSION["departmentName"] = $objResult["departmentName"];
-			if($objResult["role"] == "HR")
-			{
-				header("location:HR/index.php");
-			}
-			elseif($objResult["role"] == "Manager")
-			{
-				header("location:Manager/index.php");
-			}else {
-				header("location:Employee/index.php");
-			}
-	}
-	mysqli_close($con);
+}
+mysqli_close($con);
 ?>
--- connect.php
+++ connect.php
@@ -1,14 +1,13 @@
 <?php
 session_start();
-$serverName = "localhost";
-$userName = "root";
-$userPassword = "";
-$dbName = "testweb";
-$conn= mysqli_connect($serverName,$userName,$userPassword,$dbName) or die("Failed to connec to databse :" . mysqli_error($objCon));
-if (mysqli_connect_errno())
-	{
-		echo "Database Connect Failed : " . mysqli_connect_error();
-		exit();
-	}
+$serverName = "db";
+$userName = "MYSQL_USER";
+$userPassword = "MYSQL_PASSWORD";
+$dbName = "MYSQL_DATABASE";
+$conn = mysqli_connect($serverName, $userName, $userPassword, $dbName) or die("Failed to connec to databse :" . mysqli_error($objCon));
+if (mysqli_connect_errno()) {
+	echo "Database Connect Failed : " . mysqli_connect_error();
+	exit();
+}
 mysqli_query($conn, "SET NAMES 'utf8' ");
 ?>
